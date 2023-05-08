6ae9fa9242bb4e201c7470438e3832d732e5b4f9
nameissh <sehajeong@gmail.com>
2023-05-08 16:18:35 +0900

    Android Studio

---
+++ build/generated/source/buildConfig/release/com/kakao/talk/theme/apeach/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.kakao.talk.theme.apeach;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.kakao.talk.theme.anne01";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 100;
+  public static final String VERSION_NAME = "1.0.0";
+}
